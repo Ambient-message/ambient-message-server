bc806f005b365aaf869d72469474535bae754e75
yuzukianzu <yuzukianzu02@gmail.com>
2023-12-05 18:13:16 +0300

    feat: di test example was added to main

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,54 @@ use std::time::SystemTime;
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::accept_async;
 use uuid::Uuid;
+use std::sync::{Arc, Mutex};
 use ::inject::*;
 
-struct Connection(isize);
 
-impl Connection {
+struct MyService()
+
+impl MyService{
     #[inject]
-    fn new(foo: isize) -> Self {
-        Self(foo)
+    fn new() -> Self {
+        Self()
+    }
+
+    fn hehe(){
+        println!("hehe");
     }
 }
 
-
-struct Instance(isize);
-
-impl Instance {
-    #[inject]
-    fn new(conn: &Connection) -> Self {
-        Self(conn.0)
-    }
+struct MyService2 {
+    myService : MyService
 }
 
+impl MyService2 {
+    #[inject]
+    fn new(myService: MyService) -> Self {
+        Self{
+            myService : myService
+        }
+    }
+
+    fn foo(&self){
+        MyService::hehe();
+    }
+
+}
 
 
 
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-   
+
+   let myService = Box::new(MyService());
+   let container = container![
+        ref myService
+    ];
+
+   let instance = get!(&container, MyService2).unwrap();
+   instance.foo();
+
     Ok(())
 }
