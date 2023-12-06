6bffb1b64eb2cd78022c4ef2b4f84f0c5531a796
yuzukianzu <yuzukianzu02@gmail.com>
2023-12-06 18:42:01 +0300

    feat: user_service has been added

--- src/adapters/services/user_service.rs
+++ src/adapters/services/user_service.rs
@@ -1,4 +1,19 @@
-use crate::{domain::user::{user_repository_abstract::UserRepositoryAbstract, user::User}, application::users::requests::create_user_request::CreateUserRequest, adapters::spi::db::schema::users::password};
+use std::rc::Rc;
+
+use crate::{domain::user::{user_repository_abstract::UserRepositoryAbstract, user::User}, application::users::{requests::create_user_request::CreateUserRequest, handlers::create_user_handler::{self, CreateUserHandler}}, adapters::spi::db::schema::users::password};
 use di::injectable;
 use uuid::Uuid;
 
+
+#[injectable]
+pub struct UserService{
+    create_user_handler : Rc<CreateUserHandler>
+}
+
+
+impl UserService {
+    pub fn save(&self, create_user_request : CreateUserRequest){
+
+        self.create_user_handler.execute(create_user_request);
+    }
+}
