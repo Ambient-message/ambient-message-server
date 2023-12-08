e1927068e8547813831b5302b0cad72ee51eeb3a
yuzukianzu <yuzukianzu02@gmail.com>
2023-12-08 15:55:07 +0300

    feat: more-config/options was added to di

--- src/adapters/spi/db/db_connection.rs
+++ src/adapters/spi/db/db_connection.rs
@@ -1,26 +1,37 @@
-use std::env;
-use di::{injectable};
-use diesel::{pg::PgConnection, r2d2::ConnectionManager};
+use std::{default, rc::Rc};
 
+use config::ext::ConfigurationBinder;
+use di::{injectable, Ref};
+use diesel::{pg::PgConnection, r2d2::ConnectionManager};
+use options::Options;
+use serde::*;
+use serde_json::*;
 
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-#[injectable(DbContext)]
-#[derive(Default)]
-pub struct DbConnection {
+#[derive(Debug)]
+pub struct DbConnectionOptions {
     pub database_url: String,
 }
 
+
+impl DbConnectionOptions {
+    pub fn new(database_url: String) -> Self { Self { database_url } }
+}
+
+#[injectable(DbContext)]
+pub struct DbConnection {
+    options: Ref<dyn Options<DbConnectionOptions>>,
+}
 pub trait DbContext {
     fn get_pool(&self) -> DbPool;
 }
 
 impl DbContext for DbConnection {
-
     fn get_pool(&self) -> DbPool {
-
-        let manager = ConnectionManager::<PgConnection>::new("postgres://postgres:hiWGNPEmtNmtR4U@localhost/postgres");
-
-        r2d2::Pool::new(manager).unwrap()
+        r2d2::Pool::new(
+            ConnectionManager::<PgConnection>::new(
+                &self.options.value().database_url)
+        ).unwrap()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ambient_message_server_lib::adapters::services::service_builder::ServiceBuilder;
 use ambient_message_server_lib::adapters::services::user_service::UserService;
 use ambient_message_server_lib::application::users::requests::create_user_request::CreateUserRequest;
-use ambient_message_server_lib::adapters::spi::db::db_connection::{DbConnection, DbContext, self};
+use ambient_message_server_lib::adapters::spi::db::db_connection::{DbConnection, DbContext, self, DbConnectionOptions};
 use ambient_message_server_lib::application::users::handlers::create_user_handler::CreateUserHandler;
 use ambient_message_server_lib::domain::user::user_repository_abstract::UserRepositoryAbstract;
 use ambient_message_server_lib::infrastructure::repositories::user_repository::UserRepository;
@@ -15,23 +15,35 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::accept_async;
 use uuid::Uuid;
 use std::sync::{Arc, Mutex};
+use config::{*, ext::*};
 use di::*;
-
+use options::{*, ext::*};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
 
+
+    let config : Rc<dyn Configuration> =Rc::from(
+        DefaultConfigurationBuilder::new()
+            .add_json_file("appsettings.json")
+            .build()
+            .unwrap()
+            .as_config());
+
     let builder = ServiceBuilder::new();
 
-    let mut provider = builder
-        .add_adapters()
-        .add_application()
-        .add_infrastructure()
-        .build().build_provider().unwrap();
+    let provider = builder
+    .add_adapters()
+    .add_infrastructure()
+    .add_application()
+    .build()
+    .apply_config_at::<DbConnectionOptions>(config, "data")
+    .build_provider().unwrap();
 
 
-    let instance = provider.get_required::<UserService>();
+    let service = provider.get_required::<UserService>();
 
-    instance.save(CreateUserRequest::new("root", "1234"));
+   service.save(CreateUserRequest::new("stas", "test"));
+
     Ok(())
 }
