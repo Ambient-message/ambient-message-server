925b1214b518dc65efd4d30754dfbfea8bb41ea3
don't have any <vlad.luckashonok@ya.ru>
2023-12-12 19:38:25 +0300

    clear project

--- src/domain/user/user_repository_abstract.rs
+++
@@ -1,8 +0,0 @@
-use crate::domain::user::user::User;
-use mockall::*;
-use std::error::Error;
-
-#[automock]
-pub trait UserRepositoryAbstract {
-    fn save(&self, user : User) -> Result<User, Box<dyn Error>>;
-}
