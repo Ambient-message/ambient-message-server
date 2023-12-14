7b8d81a4cd6b11aa884829ef63fc64b4185c867d
Stas <yuzukianzu02@gmail.com>
2023-12-14 15:47:52 +0300

    delete crates/application/target directory

--- crates/application/target/debug/build/libc-fa1b234de2eacc58/build_script_build-fa1b234de2eacc58.d
+++
@@ -1,5 +0,0 @@
-/home/administrator/self/Projects/ambient-message-server/crates/application/target/debug/build/libc-fa1b234de2eacc58/build_script_build-fa1b234de2eacc58: /home/administrator/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs
-
-/home/administrator/self/Projects/ambient-message-server/crates/application/target/debug/build/libc-fa1b234de2eacc58/build_script_build-fa1b234de2eacc58.d: /home/administrator/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs
-
-/home/administrator/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs:
